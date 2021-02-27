db60fb4902a32a2b89b14c1d2f521fd3ac9242e2
abhixv <abhixv>
2021-02-27 20:57:53 +0000

    Common.mk

--- p404_begonia.mk
+++ p404_begonia.mk
@@ -27,7 +27,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, device/redmi/begonia/device.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/404/config/common_full_phone.mk)
+$(call inherit-product, vendor/404/configs/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := begonia
